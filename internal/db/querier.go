a2e003fa1cb9bc2147272bf1ab8456dab48fcab3
Michelangelo Mori <mmori@stacklok.com>
2024-09-16 17:45:20 +0200

    Save properties in batch during Get Profile.

--- internal/db/querier.go
+++ internal/db/querier.go
@@ -268,6 +268,7 @@ type Querier interface {
 	UpsertInstallationID(ctx context.Context, arg UpsertInstallationIDParams) (ProviderGithubAppInstallation, error)
 	UpsertLatestEvaluationStatus(ctx context.Context, arg UpsertLatestEvaluationStatusParams) error
 	UpsertProfileForEntity(ctx context.Context, arg UpsertProfileForEntityParams) (EntityProfile, error)
+	UpsertProperties(ctx context.Context, props json.RawMessage) error
 	UpsertProperty(ctx context.Context, arg UpsertPropertyParams) (Property, error)
 	UpsertPullRequest(ctx context.Context, arg UpsertPullRequestParams) (PullRequest, error)
 	// Copyright 2024 Stacklok, Inc

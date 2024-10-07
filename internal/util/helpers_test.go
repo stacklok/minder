00dd4ba9cfb933088e49aa55ba2f564c18dd3af6
Kugamoorthy Gajananan <gajananan@gmail.com>
2024-10-07 20:47:37 +1100

    Add additional test coverage for helper functions in Util Currently helper functions in Util lacks test coverage
    This commit fixes the lint errors after fixing lint version.
    
    Signed-off-by: Kugamoorthy Gajananan <gajananan@gmail.com>

--- internal/util/helpers_test.go
+++ internal/util/helpers_test.go
@@ -33,10 +33,11 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	pb "github.com/stacklok/minder/pkg/api/protobuf/go/minder/v1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/proto"
+
+	pb "github.com/stacklok/minder/pkg/api/protobuf/go/minder/v1"
 )
 
 // TestGetConfigDirPath tests the GetConfigDirPath function
@@ -419,9 +420,9 @@ func TestLoadCredentials(t *testing.T) {
 }
 
 // TestRevokeToken tests the RevokeToken function
-func TestRevokeToken(t *testing.T) {
+// func TestRevokeToken(t *testing.T) {
 
-}
+// }
 
 // TestGetJsonFromProto tests the GetJsonFromProto function
 func TestGetJsonFromProto(t *testing.T) {
